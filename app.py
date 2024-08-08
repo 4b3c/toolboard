02754a7f3acaa460431bf8c64f4b589d8adcfd50
Abram <58718191+4b3c@users.noreply.github.com>
2024-08-07 17:10:39 -0700

    changed layout to project (thematic yk)

--- app.py
+++ app.py
@@ -19,14 +19,14 @@ class Tool(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), nullable=False, unique=True)
 
-class Layout(db.Model):
+class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     tools = db.Column(db.JSON, nullable=False)
 
 @app.route('/')
 def home():
-    layouts = Layout.query.all()
-    return render_template('home.html', layouts=layouts)
+    projects = Project.query.all()
+    return render_template('home.html', projects=projects)
 
 @app.route('/create_tool', methods=['GET', 'POST'])
 def create_tool():
@@ -36,41 +36,41 @@ def create_tool():
         try:
             db.session.add(new_tool)
             db.session.commit()
-            flash('Tool saved successfully!', 'success')
+            flash('Tool created successfully!', 'success')
         except Exception as e:
             db.session.rollback()
-            flash('Error saving tool: ' + str(e), 'danger')
+            flash('Error creating tool: ' + str(e), 'danger')
 
     tools = Tool.query.all()
     return render_template('create_tool.html', tools=tools)
 
-@app.route('/create_layout', methods=['GET', 'POST'])
-def create_layout():
+@app.route('/create_project', methods=['GET', 'POST'])
+def create_project():
     if request.method == 'POST':
         tools_json = request.form.get('tools')
         tools_list = json.loads(tools_json)
         tools_list = [{k: int(v) if k != 'tool_name' else v for k, v in tool_dict.items()} for tool_dict in tools_list]
 
-        new_layout = Layout(tools=tools_list)
+        new_project = Project(tools=tools_list)
 
         if tools_list == []:
-            new_layout = None
+            new_project = None
 
         try:
-            db.session.add(new_layout)
+            db.session.add(new_project)
             db.session.commit()
-            flash('Tool saved successfully!', 'success')
+            flash('Project created successfully!', 'success')
         except Exception as e:
             db.session.rollback()
-            flash('Error saving tool: ' + str(e), 'danger')
+            flash('Error creating project: ' + str(e), 'danger')
 
     tools = Tool.query.all()
-    layouts = Layout.query.all()
-    return render_template('create_layout.html', tools=tools, layouts=layouts)
+    projects = Project.query.all()
+    return render_template('create_project.html', tools=tools, projects=projects)
 
-@app.route('/project/<int:layout_id>')
-def project(layout_id):
-    tools = Layout.query.get_or_404(layout_id).tools
+@app.route('/project/<int:project_id>')
+def project(project_id):
+    tools = Project.query.get_or_404(project_id).tools
     print(tools)
 
     return render_template('/project.html', tools=tools)
